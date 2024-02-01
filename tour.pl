e9e4f6418110c013704a68ca11c192139ad9bdff
adiswami14 <adithya1431@gmail.com>
2024-02-01 00:15:49 -0500

    Setup read and write issues

---
+++ tour.pl
@@ -0,0 +1,14 @@
+setup :-
+    write('Enter the number of rows in chessboard: '),
+    read(Rows),
+    write('Enter the number of columns in chessboard: '),
+    read(Columns),
+    write('What would you like the row position of the knight to be?'),
+    read(X),
+    write('What would you like the column position of the knight to be?'),
+    read(Y).
+    
+    % writeln(Rows),
+    % writeln(Columns),
+    % writeln(X),
+    % writeln(Y).
