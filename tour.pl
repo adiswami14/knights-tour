4596414908908cb5fb4bf315d9eb115e16d0d493
adiswami14 <adithya1431@gmail.com>
2024-02-03 19:46:22 -0500

    Fix issues with appending to a list and displaying tour

--- tour.pl
+++ tour.pl
@@ -8,42 +8,48 @@ locationValid(I, J) :-
 
 % tracks all the possible moves the knight can make at location (I, J)
 possibleMoves(I, J, NextI, NextJ) :-
-    ((NextI is I+1, NextJ is J+2);
-    (NextI is I+1, NextJ is J-2);
-    (NextI is I-1, NextJ is J-2);
-    (NextI is I-1, NextJ is J+2);
-    (NextI is I+2, NextJ is J+1);
-    (NextI is I+2, NextJ is J-1);
-    (NextI is I-2, NextJ is J-1);
-    (NextI is I-2, NextJ is J+1)).
-
-% creates a list of size S, all zeros
-createEmptyList(0, []) :- !.
-createEmptyList(S, [H|T]) :-
-    H is 0,
-    NewS is S-1,
-    createEmptyList(NewS, T).
-
-% changes list val at index I to value
-changeListVal([H|T], 0, Value) :-
-    H is Value.
-changeListVal([H|T], Index, Value) :-
-    Ind is Index-1,
-    changeListVal(T, Ind, Value).
-
-% updates the list that can track how many moves available at spot (I, J) in chessboard (so Warnsdorff's heuristic can be used)
+    NextI is I+1, NextJ is J+2;
+    NextI is I+1, NextJ is J-2;
+    NextI is I-1, NextJ is J-2;
+    NextI is I-1, NextJ is J+2;
+    NextI is I+2, NextJ is J+1;
+    NextI is I+2, NextJ is J-1;
+    NextI is I-2, NextJ is J-1;
+    NextI is I-2, NextJ is J+1.
 
 % checks the number of possible moves from (I, J) is Val
 checkPossibleMoves(I, J, L, Val) :-
     chessboard(Rows, Columns),
-    setof(_, hasMovePossible(I, J, L), PossibleMoves), 
-    length(PossibleMoves, Val).
-    % convertToListIndex(I, J, Rows, Columns, Index),
-    % changeListVal(L, Index, NumPossibleMoves).
+    setof(_, hasMovePossible(I, J, L), PossibleMoves),
+    getLength(PossibleMoves, Val).  % Val is length of PossibleMoves
 
-makeMove() :- !.
+% creates a list of size S, all zeros
+createEmptyList(0, L) :- !.
+createEmptyList(S, [[0, 0]|T]) :-
+    NewS is S-1,
+    createEmptyList(NewS, T).
+
+% switch to append to list
+append([], L, L).
+append([H|T], L, [H|R]) :-
+    append(T, L, R).
+
+% Make optimal move by picking move with least amount of possible moves
+makeOptimalMove(I, J, L, NewI, NewJ) :- 
+    setof((Number, Locations), setof(checkPossibleMoves(I, J, L, Val), hasMovePossible(I, J, L), Locations), NextMoves),
+    (NewI, NewJ) = Locations.
+
+% Get length of a set
+getLength([], Length) :- !.
+getLength([H|T], Length) :- 
+    L is Length+1,
+    getLength(T, L).
 
 % displays final output of tour
+displayTour([]) :- !.
+displayTour([H|T]) :-
+    writeln(H),
+    displayTour(T).
 
 % Counting number of possible moves from location (I, J)
 hasMovePossible(I, J, L) :-
@@ -54,9 +60,10 @@ hasMovePossible(I, J, L) :-
 notBeenVisited(I, J, L) :-
     chessboard(Rows, Columns),
     locationValid(I, J),
-    convertToListIndex(I, J, Rows, Columns, Index),
-    nth0(Index, L, Val),
-    Val =:= 0.  % Looking if value at the index hasn't been touched yet
+    \+ member([I, J], L).
+    % convertToListIndex(I, J, Rows, Columns, Index),
+    % nth0(Index, L, Val),
+    % Val =:= [0, 0].  % Looking if value at the index hasn't been touched yet
 
 % converts 2-D position in chessboard to list index
 convertToListIndex(I, J, N, M, ListIndex) :-
@@ -83,8 +90,17 @@ setup :-
     assert(ChessBoard),
     assert(Position).
 
-%     % writeln(Rows),
-%     % writeln(Columns),
-%     % writeln(X),
-%     % writeln(Y).
-% N, M, I, J are just filler variable names for the rules, the actual values are Rows, Columns, X, and Y
+startTour :-
+    chessboard(Rows, Columns),
+    position(X, Y),
+    TotalMoves is (Rows*Columns),
+    append(Tour, [[X, Y]], AppendedTour),
+    % createTour(AppendedTour, X, Y, 1, TotalMoves),
+    displayTour(AppendedTour).
+
+createTour(Tour, I, J, MoveCount, TotalMoves) :-
+    MoveCount =< TotalMoves,
+    % makeOptimalMove(I, J, Tour, NewI, NewJ),
+    append(Tour, [[NewI, NewJ]], AppendedTour),
+    MoveIncrement is MoveCount+1,
+    createTour(AppendedTour, NewI, NewJ, MoveIncrement, TotalMoves).
